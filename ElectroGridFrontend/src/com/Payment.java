74cc1a5276deb3e496ab8fbee46fdc4150de67d1
Ushanii <ushanisdahanayake@gmail.com>
2022-05-15 10:14:08 +0530

    Add comments

--- ElectroGridFrontend/src/com/Payment.java
+++ ElectroGridFrontend/src/com/Payment.java
@@ -2,12 +2,18 @@ package com;
 
 import java.sql.*;
 
-public class Payment  {
+public class Payment {
+	
+	// To connect to the DB
 	private Connection connect() {
+		
 		Connection con = null;
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+			// Provide the correct details: DBServer/DBName, username, password
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf", "root", "mysql");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -15,120 +21,195 @@ public class Payment  {
 	}
 
 	
+	// read payment details
+	
 	public String readPayment() {
+		
 		String output = "";
+		
 		try {
+			
+			// database connection
 			Connection con = connect();
+			
+			// error while connecting
 			if (con == null) {
 				return "Error while connecting to the database for reading.";
 			}
+			
 			// Prepare the html table to be displayed
-			output = "<table border='1'><tr><th>Account Number</th>"+ "<th>Payment Amount</th>"
+			output = "<table border='1'><tr><th>Account Number</th>" + "<th>Payment Amount</th>"
 					+ "<th>Payment Date</th>" + "<th>Update</th>" + "<th>Delete</th>";
+			
 			String query = "select * from payment";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
+			
 			// iterate through the rows in the result set
 			while (rs.next()) {
+				
 				String paymentId = Integer.toString(rs.getInt("paymentId"));
 				String accountNum = rs.getString("accountNum");
 				String amount = Double.toString(rs.getDouble("amount"));
 				String date = rs.getString("date");
+				
 				// Add into the html table
-				output += "<tr><td><input id='hidPaymentIDUpdate' name='hidPaymentIDUpdate' type='hidden' value='" + paymentId
-						+ "'>" + accountNum + "</td>";
+				output += "<tr><td><input id='hidPaymentIDUpdate' name='hidPaymentIDUpdate' type='hidden' value='"
+						+ paymentId + "'>" + accountNum + "</td>";
 				output += "<td>" + amount + "</td>";
 				output += "<td>" + date + "</td>";
-				
+
 				// buttons
 				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
-						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-paymentid='"+paymentId+"'>" + "</td></tr>";
+						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-paymentid='"
+						+ paymentId + "'>" + "</td></tr>";
 			}
 			con.close();
+			
 			// Complete the html table
 			output += "</table>";
+			
 		} catch (Exception e) {
-			output = "Error while reading the items.";
+			
+			// error while reading
+			output = "Error while reading payment details!";
 			System.err.println(e.getMessage());
 		}
+		
 		return output;
 	}
 
+	
+	// insert payment details
+	
 	public String insertPayment(String account, String amount, String date) {
+		
 		String output = "";
+		
 		try {
+			
+			// database connection
 			Connection con = connect();
+			
+			// error while connecting
 			if (con == null) {
 				return "Error while connecting to the database for inserting.";
 			}
+			
 			// create a prepared statement
 			String query = " insert into payment(`paymentId`,`accountNum`,`amount`,`date`)" + " values (?, ?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
 			// binding values
 			preparedStmt.setInt(1, 0);
-			preparedStmt.setString(2, account);			
+			preparedStmt.setString(2, account);
 			preparedStmt.setDouble(3, Double.parseDouble(amount));
 			preparedStmt.setString(4, date);
+			
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
+			
 			String newPayment = readPayment();
+			
+			// insert success message
 			output = "{\"status\":\"success\", \"data\": \"" + newPayment + "\"}";
+			
 		} catch (Exception e) {
-			output = "{\"status\":\"error\", \"data\":	 \"Error while inserting the item.\"}";
+			
+			// error while inserting
+			output = "{\"status\":\"error\", \"data\":	 \"Error while inserting payment details!\"}";
 			System.err.println(e.getMessage());
 		}
-		return output;
-	}
-
-	public String updatePayment(String ID, String account, String amount, String date) {
-		String output = "";
-		try {
-			Connection con = connect();
-			if (con == null) {
-				return "Error while connecting to the database for updating.";
-			}
-			// create a prepared statement
-			String query = "UPDATE payment SET accountNum=?,amount=?,date=?WHERE paymentId=?";
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			// binding values
-			preparedStmt.setString(1, account);			
-			preparedStmt.setDouble(2, Double.parseDouble(amount));
-			preparedStmt.setString(3, date);
-			preparedStmt.setInt(4, Integer.parseInt(ID));
-			// execute the statement
-			preparedStmt.execute();
-			con.close();
-			String newPayment = readPayment();
-			output = "{\"status\":\"success\", \"data\": \"" + newPayment + "\"}";
-		} catch (Exception e) {
-			output = "{\"status\":\"error\", \"data\": \"Error while updating the item.\"}";
-			System.err.println(e.getMessage());
-		}
+		
 		return output;
 	}
 	
-	public String deletePayment(String paymentId) {
+	
+	// update payment details
+
+	public String updatePayment(String ID, String account, String amount, String date) {
+		
 		String output = "";
+		
 		try {
+			
+			// database connection
 			Connection con = connect();
+			
+			// error while connecting
 			if (con == null) {
-				return "Error while connecting to the database for deleting.";
+				return "Error while connecting to the database for updating.";
 			}
+			
 			// create a prepared statement
-			String query = "delete from payment where paymentId=?";
+			String query = "UPDATE payment SET accountNum=?,amount=?,date=?WHERE paymentId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
 			// binding values
-			preparedStmt.setInt(1, Integer.parseInt(paymentId));
+			preparedStmt.setString(1, account);
+			preparedStmt.setDouble(2, Double.parseDouble(amount));
+			preparedStmt.setString(3, date);
+			preparedStmt.setInt(4, Integer.parseInt(ID));
+			
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
+			
 			String newPayment = readPayment();
+			
+			// update success message
 			output = "{\"status\":\"success\", \"data\": \"" + newPayment + "\"}";
+			
 		} catch (Exception e) {
+			
+			// error while updating
+			output = "{\"status\":\"error\", \"data\": \"Error while updating the payment!\"}";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	
+	// delete payment details
+
+	public String deletePayment(String paymentId) {
+		
+		String output = "";
+		
+		try {
+			
+			// database connection
+			Connection con = connect();
+			
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			
+			// create a prepared statement
+			String query = "delete from payment where paymentId=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(paymentId));
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			String newPayment = readPayment();
+			
+			// delete success message
+			output = "{\"status\":\"success\", \"data\": \"" + newPayment + "\"}";
+			
+		} catch (Exception e) {
+			
+			// error while deleting
 			output = "{\"status\":\"error\", \"data\": \"Error while deleting the item.\"}";
 			System.err.println(e.getMessage());
 		}
+		
 		return output;
 	}
 
--- ElectroGridFrontend/src/com/PaymentsAPI.java
+++ ElectroGridFrontend/src/com/PaymentsAPI.java
@@ -26,46 +26,60 @@ public class PaymentsAPI extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
     
+    
+    // insert payments
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String output = paymentObj.insertPayment(request.getParameter("accountNum"),
-				 request.getParameter("amount"),				
-				request.getParameter("date"));
-				response.getWriter().write(output);
+				 		request.getParameter("amount"),				
+				 		request.getParameter("date"));
 		
+		response.getWriter().write(output);		
 	}
     
+    
+    // Convert request parameters to a Map
     private static Map getParasMap(HttpServletRequest request) {
+    	
 		Map<String, String> map = new HashMap<String, String>();
+		
 		try {
 			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
 			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
 			scanner.close();
 			String[] params = queryString.split("&");
+			
 			for (String param : params) {
 
 				String[] p = param.split("=");
 				map.put(p[0], p[1]);
 			}
+			
 		} catch (Exception e) {
 		}
 		return map;
 	}
 
+    
+    // update payments
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Map paras = getParasMap(request);
-		 String output = paymentObj.updatePayment(paras.get("hidPaymentIDSave").toString(),
-		 paras.get("accountNum").toString(),
-		paras.get("amount").toString(),
-		paras.get("date").toString());		
+		String output = paymentObj.updatePayment(paras.get("hidPaymentIDSave").toString(),
+				 			paras.get("accountNum").toString(),
+				 			paras.get("amount").toString(),
+				 			paras.get("date").toString());	
+		 
 		response.getWriter().write(output);
 	}
 
+    
+    // delete payments
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Map paras = getParasMap(request);
-		 String output = paymentObj.deletePayment(paras.get("paymentId").toString());
+		String output = paymentObj.deletePayment(paras.get("paymentId").toString());
+		 
 		response.getWriter().write(output); 
 	}
 
