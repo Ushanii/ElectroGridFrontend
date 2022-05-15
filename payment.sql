cd8cedfe56186e5fab6ab0d4f86ac32c137c7c14
Ushani S Dahanayake <88320214+Ushanii@users.noreply.github.com>
2022-05-15 17:40:15 +0530

    Add files via upload

---
+++ payment.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM paf.payment
+LIMIT 0, 1000
+
+-- Date: 2022-05-15 17:38
+*/
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (45,999745,2000,'2020-01-20');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (46,99745,800,'2020-01-02');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (47,40026,1000,'2022-04-05');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (48,54100,4500,'2022-04-10');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (49,54119,2000,'2022-04-24');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (50,999746,500,'2021-05-11');
+INSERT INTO `` (`paymentId`,`accountNum`,`amount`,`date`) VALUES (52,999600,500,'2022-05-10');
